b0074e2b8ddc2b028fdf5ccb0b2c10890050c9f1
Illuminaty666 <132865731+Illuminaty666@users.noreply.github.com>
2025-04-08 03:12:34 +0000

    aa

--- problem_1_calculator.py
+++ problem_1_calculator.py
@@ -1,7 +1,16 @@
 from testhelper import test
 
 def calculate(input1, operator, input2):
-    pass #Replace with your code
+    if operator == "+":
+        result == input1 + input2
+    elif operator == "-":
+        result = input1 - input2
+    elif operator == "*":
+        result = input1 * input2
+    else:
+        result = input1 / input2
+
+
 
 
 

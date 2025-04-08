de57fec507a2cbaecb18e907900522a477458b1e
Illuminaty666 <132865731+Illuminaty666@users.noreply.github.com>
2025-04-08 03:56:37 +0000

    p4

--- problem_3_top_wordle_letters.py
+++ problem_3_top_wordle_letters.py
@@ -10,6 +10,25 @@ def get_valid_wordle_words():
         str_words.append(str(word.decode('ascii')))
     return str_words
 
+def letter_freq(words, letter):
+    freq = {}
+    for word in words:
+        count = word.lower().count(letter.lower())
+        freq[word] = count
+    return freq
+
+def find_most_used_letter(freq):
+    most_used_letter = max(freq, key=freq.get)
+    return most_used_letter, freq[most_used_letter]
+
+wlist = get_valid_wordle_words()
+letter_to_count = input("Desired letter to count: ")
+
+freq = letter_freq(wlist, letter_to_count)
+most_used_letter, count = find_most_used_letter(freq)
+
+print(f"The most used letter '{most_used_letter}' occurs {count} times.")
+
 # all_words will contains a list of all valid words that you can use
 all_words = get_valid_wordle_words()
 
--- problem_4_magic_squares.py
+++ problem_4_magic_squares.py
@@ -1,21 +1,20 @@
 from testhelper import test
 
 def is_square_magic(square):
-    pass # Replace this with your code
-
-
+    num = square[0][0] + square[0][1] + square[0][2]
+    if square[0][0] + square[0][1] + square[0][2] == num and square[1][0] + square[1][1] + square[1][2] == num and square[2][0] + square[2][1] + square[2][2] == num and square[0][0] + square[1][0] + square[2][0] == num and square[0][1] + square[1][1] + square[2][1] == num and square[0][2] + square[1][2] + square[2][2] == num:
+        return("True")       
+    else:
+        return("False")
 
 
 
 
 ### TESTS - Run the code that calls the function to check it works.
-valid_square = [[4, 9, 2], 
-          [3, 5, 7], 
-          [8, 1, 6]]
+valid_square = [[4, 9, 2],[3, 5, 7],[8, 1, 6]]
 
 invalid_square = [[4, 9, 2], 
-          [8, 1, 6],
-          [3, 5, 7]]
-
+                [8, 1, 6],
+                [3, 5, 7]]
 test("Magic Square 1", True, is_square_magic(valid_square))
 test("Magic Square 2", False, is_square_magic(invalid_square))
